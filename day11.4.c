20dadda2ed3516ad9fd1efd73fff5ce318182124
kevin1267787 <95349596+kevin1267787@users.noreply.github.com>
2023-06-07 03:20:27 -0700

    Add files via upload

---
+++ day11.4.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main (){
+	
+	char a;
+	
+	printf("Enter value of a:");
+	scanf("%c",&a);
+
+    switch(a){
+    	
+	
+		
+		case 'M' :
+			printf("Monday");
+		break;
+			
+		case 'T' :
+			printf("Tuesday");
+		break;
+			
+		case 'W' :
+			printf("Wednesday");
+		break;
+			
+		case 'H' :
+			printf("Thursday");
+		break;
+			
+		case 'F' :
+			printf("Friday");
+		break;
+			
+		case 'S' :
+			printf("Saturday");
+		break;
+			
+		case 'U' :
+			printf("Sunday");
+		break;
+			
+		
+		
+		
+		    
+	}
+	
+	return 0;
+}
