e0fa3a85de653e547d3ac6c0398c851ca2009268
kevin1267787 <95349596+kevin1267787@users.noreply.github.com>
2023-06-16 07:46:06 -0700

    Add files via upload

---
+++ pr.4circlet/pro.16.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+	
+	int r,c,k;
+	
+	for(r=1;r<=5;r++){
+		for(k=r;k<=4;k++){
+			printf(" ");
+		}
+		for(c=1;c<=r;c++){
+			printf("%d",c);
+		}
+		printf("\n");
+	}
+
+for(r=1;r<=5;r++){
+		for(k=1;k<=r;k++){
+			printf(" ");
+		}
+		for(c=r;c<5;c++){
+			printf("%d",c);
+		}
+		printf("\n");
+	}
+	
+	
+	return 0;
+}
+
