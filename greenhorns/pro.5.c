03df223c7be22c048af748e99b532b8dbf981a97
kevin1267787 <95349596+kevin1267787@users.noreply.github.com>
2023-06-19 00:54:49 -0700

    Add files via upload

---
+++ greenhorns/pro.5.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+	
+	int c;
+	float f;
+	
+	printf("Enter value of c:");
+	scanf("%d",&c);
+	
+	f = (9/5)*c + 32;
+	
+	printf("f:%.2f",f);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
