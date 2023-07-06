3f9a026bb0eb5ffd724922636ae4ae9f6748e3b1
kevin1267787 <95349596+kevin1267787@users.noreply.github.com>
2023-07-06 08:57:43 -0700

    Add files via upload

---
+++ File handling/demo1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	
+	FILE *p;
+	
+	char name[20];
+	
+	p = fopen("Demo.txt","w");
+	
+	if(p == NULL){
+		printf("Error");
+	}
+	else{
+		printf("Enter Text :");
+		gets(name);
+		
+		fputs(name,p);
+		fclose(p);
+	}
+	return 0;
+}
---
+++ File handling/demo3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+	
+	FILE *p;
+	
+	char name[20];
+	
+	p = fopen("Demo.txt","a");
+	
+	if(p == NULL){
+		printf("Error");
+	}
+	else{
+		printf("Enter Text :");
+		gets(name);
+		
+		
+		fputs(name,p);
+		fputs("\n",p);
+		fclose(p);
+	}
+	return 0;
+}
